8161f093debeb4b1fe8654f838c5e633f0f6c2d8
Nir Parisian <nir.parisian@gmail.com>
2022-02-03 11:10:07 +0200

    everything

--- pages/api/todos.js
+++ pages/api/todos.js
@@ -1,9 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 const todos = require('../../lib/todos');
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   console.log(req.body);
   todos.addTodo(req.body);
-  res.status(200).json(todos.listTodos());
+  res.status(200).json(await todos.listTodos());
 }
-
--- pages/todos.js
+++ pages/todos.js
@@ -1,15 +1,17 @@
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.css';
 import { useState } from 'react';
 import { listTodos } from '../lib/todos';
 import { useRouter } from 'next/router';
+import { db } from '../firebase/clientApp';
+import { doc, updateDoc } from 'firebase/firestore';
 
 export async function getServerSideProps(context) {
   console.log(context);
   return {
     props: {
-      todos: listTodos(),
+      todos: await listTodos(),
     },
-  }
+  };
 }
 
 export default function Todos(props) {
@@ -19,30 +21,28 @@ export default function Todos(props) {
 
   async function handleSubmit(e) {
     e.preventDefault();
-    fetch('/api/todos', {
-      method: 'POST',
-      body: JSON.stringify(text),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    }).then(() => {
-       router.replace(router.asPath);
-    });
+    const todosRef = doc(db, 'nirparisian', 'todos');
+    await updateDoc(todosRef, { [text]: false }, { merge: true });
+
+    router.replace(router.asPath);
   }
 
   return (
     <div className={styles.container}>
       <p>My Todo App</p>
       <form onSubmit={handleSubmit}>
-        <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
         <input type="submit" value="Submit" />
       </form>
       <ul>
-        {todos.map((todo, idx) => (
+        {Object.keys(todos).map((todo, idx) => (
           <li key={idx}>{todo}</li>
         ))}
       </ul>
     </div>
-  )
+  );
 }
-
